fa9b685f410e523e4fdd266ddad44994f992e1f7
sushantpaudel77 <sushantpaudel77@gmail.com>
2025-07-05 18:56:23 +0545

    Polish addfood components

--- admin-panel/src/pages/AddFood/AddFood.jsx
+++ admin-panel/src/pages/AddFood/AddFood.jsx
@@ -1,15 +1,42 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { assets } from "../../assets/assets";
 
 const AddFood = () => {
   const [image, setImage] = useState(false);
+  const [data, setData] = useState({
+    name: "",
+    description: "",
+    price: "",
+    category: "Biryani",
+  });
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
+  const onChangeHandler = (event) => {
+    const { name, value } = event.target; // extract the input field's name and value
+    setData((prevData) => ({
+      ...prevData, // copy previous state
+      [name]: value, // update the specific field
+    }));
+  };
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    if (!image) {
+      alert("Please select an image.");
+      return;
+    }
+  };
+
   return (
     <div className="mx-2 mt-2">
       <div className="row">
         <div className="card col-md-4">
           <div className="card-body">
             <h2 className="mb-4">Add Food</h2>
-            <form>
+            <form onSubmit={onSubmitHandler}>
               <div className="mb-3">
                 <label htmlFor="image" className="form-label">
                   <img
@@ -22,7 +49,6 @@ const AddFood = () => {
                   type="file"
                   className="form-control"
                   id="image"
-                  required
                   hidden
                   onChange={(e) => setImage(e.target.files[0])}
                 />
@@ -37,6 +63,8 @@ const AddFood = () => {
                   id="name"
                   required
                   name="name"
+                  onChange={onChangeHandler}
+                  value={data.name}
                 />
               </div>
 
@@ -49,14 +77,22 @@ const AddFood = () => {
                   id="message"
                   rows="5"
                   required
-                  name="discription"
+                  name="description"
+                  onChange={onChangeHandler}
+                  value={data.description}
                 ></textarea>
               </div>
               <div className="mb-3">
                 <label htmlFor="category" className="form-label">
                   Category
                 </label>
-                <select name="category" id="category" className="form-control">
+                <select
+                  name="category"
+                  id="category"
+                  className="form-control"
+                  onChange={onChangeHandler}
+                  value={data.category}
+                >
                   <option value="Biryani">Biryani</option>
                   <option value="Cake">Cake</option>
                   <option value="Burger">Burger</option>
@@ -76,6 +112,8 @@ const AddFood = () => {
                   name="price"
                   id="price"
                   className="form-control"
+                  onChange={onChangeHandler}
+                  value={data.price}
                 />
               </div>
               <button type="submit" className="btn btn-primary">
