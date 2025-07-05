781789f3719fef0064021db7c05119dbd09c4ffd
sushantpaudel77 <sushantpaudel77@gmail.com>
2025-07-05 18:15:40 +0545

    ADD: upload file components

--- admin-panel/src/pages/AddFood/AddFood.jsx
+++ admin-panel/src/pages/AddFood/AddFood.jsx
@@ -1,9 +1,92 @@
-import React from 'react';
+import React, { useState } from "react";
+import { assets } from "../../assets/assets";
 
 const AddFood = () => {
+  const [image, setImage] = useState(false);
   return (
-    <div>AddFood</div>
-  )
-}
+    <div className="mx-2 mt-2">
+      <div className="row">
+        <div className="card col-md-4">
+          <div className="card-body">
+            <h2 className="mb-4">Add Food</h2>
+            <form>
+              <div className="mb-3">
+                <label htmlFor="image" className="form-label">
+                  <img
+                    src={image ? URL.createObjectURL(image) : assets.upload}
+                    alt=""
+                    width={98}
+                  />
+                </label>
+                <input
+                  type="file"
+                  className="form-control"
+                  id="image"
+                  required
+                  hidden
+                  onChange={(e) => setImage(e.target.files[0])}
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="name" className="form-label">
+                  Name
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="name"
+                  required
+                  name="name"
+                />
+              </div>
+
+              <div className="mb-3">
+                <label htmlFor="message" className="form-label">
+                  Discription
+                </label>
+                <textarea
+                  className="form-control"
+                  id="message"
+                  rows="5"
+                  required
+                  name="discription"
+                ></textarea>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="category" className="form-label">
+                  Category
+                </label>
+                <select name="category" id="category" className="form-control">
+                  <option value="Biryani">Biryani</option>
+                  <option value="Cake">Cake</option>
+                  <option value="Burger">Burger</option>
+                  <option value="Pizza">Pizza</option>
+                  <option value="Rolls">Rolls</option>
+                  <option value="Salad">Salad</option>
+                  <option value="Icecream">Icecream</option>
+                </select>
+              </div>
+
+              <div className="mb-3">
+                <label htmlFor="price" className="form-label">
+                  Price
+                </label>
+                <input
+                  type="number"
+                  name="price"
+                  id="price"
+                  className="form-control"
+                />
+              </div>
+              <button type="submit" className="btn btn-primary">
+                Save
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default AddFood;
