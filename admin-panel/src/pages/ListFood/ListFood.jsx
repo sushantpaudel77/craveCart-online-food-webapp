0a1bf5a6ee336f0b3238829e441178a5532a0ef0
sushantpaudel77 <sushantpaudel77@gmail.com>
2025-07-05 21:58:23 +0545

    ADD: table for listform compo

--- admin-panel/src/pages/ListFood/ListFood.jsx
+++ admin-panel/src/pages/ListFood/ListFood.jsx
@@ -21,11 +21,44 @@ const ListFood = () => {
     }
   };
 
-  useState(() => {
+  useEffect(() => {
     fetchList();
-  });
+  }, []);
 
-  return <div>ListFood</div>;
+  return (
+    <div className="py-5 row justify-content-center">
+      <div className="col-11 card">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Image</th>
+              <th>Name</th>
+              <th>Category</th>
+              <th>Price</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {list.map((item, index) => {
+              return (
+                <tr key={index}>
+                  <td>
+                    <img src={item.imageUrl} alt="" height={48} width={48} />
+                  </td>
+                  <td>{item.name}</td>
+                  <td>{item.category}</td>
+                  <td>{`रु ${item.price}`}</td>
+                  <td className="text-danger">
+                    <i className="bi bi-x-circle-fill"></i>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default ListFood;
