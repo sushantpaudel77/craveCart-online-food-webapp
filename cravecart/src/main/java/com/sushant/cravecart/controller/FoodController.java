f8450a8b52e55d3eb895075806e145aeea79ea61
sushantpaudel77 <sushantpaudel77@gmail.com>
2025-07-05 13:26:36 +0545

    ADD: addFood controller

--- cravecart/src/main/java/com/sushant/cravecart/controller/FoodController.java
+++ cravecart/src/main/java/com/sushant/cravecart/controller/FoodController.java
@@ -1,11 +1,38 @@
 package com.sushant.cravecart.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sushant.cravecart.io.FoodRequest;
+import com.sushant.cravecart.io.FoodResponse;
+import com.sushant.cravecart.service.FoodService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping(path = "/api/foods")
 @RequiredArgsConstructor
 public class FoodController {
+
+    private final FoodService foodService;
+    private final ObjectMapper objectMapper;
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<FoodResponse> addFood(
+            @RequestPart("food") String foodString,
+            @RequestPart("file") MultipartFile file) {
+
+        FoodRequest request;
+        try {
+            request = objectMapper.readValue(foodString, FoodRequest.class);
+        } catch (JsonProcessingException ex) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid JSON format");
+        }
+        FoodResponse foodResponse = foodService.addFood(request, file);
+        return ResponseEntity.status(HttpStatus.CREATED).body(foodResponse);
+    }
 }
--- cravecart/src/main/java/com/sushant/cravecart/io/FoodRequest.java
+++ cravecart/src/main/java/com/sushant/cravecart/io/FoodRequest.java
@@ -1,9 +1,11 @@
 package com.sushant.cravecart.io;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
