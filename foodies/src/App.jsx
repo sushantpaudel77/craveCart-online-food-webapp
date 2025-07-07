412f837aa8e9f7e117f149b6854e2170b9c79a2d
sushantpaudel77 <sushantpaudel77@gmail.com>
2025-07-07 22:37:01 +0545

    ADD: contact us and explore food and home dummy components

--- foodies/src/App.jsx
+++ foodies/src/App.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Menubar from "./components/Menubar/Menubar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import ContactUs from "./pages/Contact Us/ContactUs";
 
 const App = () => {
   return (
     <div>
       <Menubar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<ContactUs />} />
+      </Routes>
     </div>
   );
 };
