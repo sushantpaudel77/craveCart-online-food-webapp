625efbeffe70b13c8c9190c09b3f10cdcf8cdb09
sushantpaudel77 <sushantpaudel77@gmail.com>
2025-07-07 23:01:39 +0545

    ADD: header component

--- foodies/src/App.jsx
+++ foodies/src/App.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import Menubar from "./components/Menubar/Menubar";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
-import ContactUs from "./pages/Contact Us/ContactUs";
 import ExploreFood from "./pages/ExploreFood/ExploreFood";
+import Contact from "./pages/Contact Us/Contact";
+import { Header } from "./components/Header/Header";
 
 const App = () => {
   return (
     <div>
       <Menubar />
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="/explore" element={<ExploreFood />} />
       </Routes>
     </div>
---
+++ foodies/src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+export const Header = () => {
+  return (
+    <div>
+      <div className="p-5 mb-4 bg-light rounded-3 mt-1">
+        <div className="container-fluid py-5">
+          <h1 className="display-5 fw-bold">Order your favourite food here</h1>
+          <p className="col-md-8 fs-4">
+            Discover the best food and drinks in Kathmandu
+          </p>
+          <Link to="/explore" className="btn btn-primary btn-lg">
+            Explore
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
